653c10ebbd40771576e64be3d9ecd38fa10ae87f
Abhijit Pandey <abhijitpandey524@gmail.com>
2025-01-07 20:07:36 +0530

    setup vectordb hf model and ingestion pipeline

--- src/pages/api/updatedatabase.ts
+++ src/pages/api/updatedatabase.ts
@@ -1,29 +1,35 @@
+"use client"
+import { updateVectorDB } from "@/../utils";
 import { Pinecone } from "@pinecone-database/pinecone";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { NextApiRequest, NextApiResponse } from "next";
+//import path from "path";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method == 'POST'){
-        const {indexname, namespace} = JSON.parse(req.body)
+    if (req.method === 'POST') {
+        const { indexname, namespace } = JSON.parse(req.body)
+        console.log("awaiting handle upload");
         await handleUpload(indexname, namespace, res);
     }
+
 }
 
 async function handleUpload(indexname: string, namespace: string, res: NextApiResponse) {
-    //throw new Error("Function not implemented.");
-    const loader = new DirectoryLoader('./documents', {
+    console.log("handle uplaod function called");
+    const loader = new DirectoryLoader('./documents',{
         '.pdf': (path: string) => new PDFLoader(path, {
             splitPages: false
         }),
-        '.txt': (path: string) => new TextLoader(path)
+        //'.txt': (path: string) => new TextLoader(path)
     });
     const docs = await loader.load();
+    console.log("loader is ready");
     const client = new Pinecone({
         apiKey: process.env.PINECONE_API_KEY!
     })
-    await updateVectorDB(client, indexname, namespace, docs, (filename, totalChunks, chunksUpserted, isComplete) => {
+    await updateVectorDB(client, indexname, namespace, docs, (filename: any, totalChunks: any, chunksUpserted: any, isComplete: any) => {
         console.log(`${filename}-${totalChunks}-${chunksUpserted}-${isComplete}`)
         if (!isComplete) {
             res.write(
@@ -38,5 +44,4 @@ async function handleUpload(indexname: string, namespace: string, res: NextApiRe
             res.end();
         }
     })
-    
 }
